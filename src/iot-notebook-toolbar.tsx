1ce4ac912ba57c1430eec449aab8b41d304549c8
Juan Sáenz <juan.saenz@polito.it>
2021-01-07 17:46:20 +0100

    The 'Board' toolbar button gathers the list of Arduinos connected

--- src/iot-notebook-toolbar.tsx
+++ src/iot-notebook-toolbar.tsx
@@ -1,33 +1,33 @@
 import React from 'react';
 
 import {
-    DocumentRegistry
+  DocumentRegistry
 } from '@jupyterlab/docregistry';
 
 import {
-    NotebookPanel, INotebookModel
+  NotebookPanel, INotebookModel
 } from '@jupyterlab/notebook';
 
 import {
-    IDisposable, DisposableDelegate
+  IDisposable, DisposableDelegate
 } from '@lumino/disposable';
 
 import {
-    ToolbarButton,
-    ReactWidget,
-    ISessionContext,
-    ToolbarButtonComponent
+  ToolbarButton,
+  ReactWidget,
+  ISessionContext,
+  ToolbarButtonComponent
 } from '@jupyterlab/apputils';
 
 import {
-    HTMLSelect,
-    LabIcon
+  HTMLSelect,
+  LabIcon
 } from '@jupyterlab/ui-components';
 
 import {
-    nullTranslator,
-    ITranslator,
-    TranslationBundle
+  nullTranslator,
+  ITranslator,
+  TranslationBundle
 } from '@jupyterlab/translation';
 
 import { IOutput } from '@jupyterlab/nbformat';
@@ -39,13 +39,13 @@ import { ISignal, Signal } from '@lumino/signaling';
 import { Notebook } from '@jupyterlab/notebook';
 
 export const board = new LabIcon({
-    name: 'defaultpkg:board',
-    svgstr: boardIconSvgStr
+  name: 'defaultpkg:board',
+  svgstr: boardIconSvgStr
 });
 
 export const boardOff = new LabIcon({
-    name: 'defaultpkg:board-off',
-    svgstr: boardOffIconSvgStr
+  name: 'defaultpkg:board-off',
+  svgstr: boardOffIconSvgStr
 });
 
 import { KernelMessage } from '@jupyterlab/services';
@@ -75,189 +75,199 @@ const TOOLBAR_KERNEL_NAME_CLASS = 'jp-Toolbar-kernelName';
  */
 export class IoTToolbar implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
 
-    /**
-     * Create a new extension object.
-     */
-    createNew(panel: NotebookPanel): IDisposable {
+  /**
+   * Create a new extension object.
+   */
+  createNew(panel: NotebookPanel): IDisposable {
 
-        let button = new ToolbarButton({
-            className: 'myButton',
-            label: 'IoT Architectural Element',
-        });
+    let button = new ToolbarButton({
+      className: 'myButton',
+      label: 'IoT Architectural Element',
+    });
 
-        let switchIoT = new IoTArchitecturalSwitch(panel.content);
-        let boardConn = new BoardConnector(panel.sessionContext);
+    let switchIoT = new IoTArchitecturalSwitch(panel.content);
+    let boardConn = new BoardConnector(panel.sessionContext);
 
-        panel.toolbar.insertItem(11, 'lblIoTAE', button);
-        panel.toolbar.insertAfter('lblIoTAE', 'switchIoTAE', switchIoT);
-        panel.toolbar.insertItem(15, 'lblBoardConn', boardConn);
+    panel.toolbar.insertItem(11, 'lblIoTAE', button);
+    panel.toolbar.insertAfter('lblIoTAE', 'switchIoTAE', switchIoT);
+    panel.toolbar.insertItem(15, 'lblBoardConn', boardConn);
 
-        return new DisposableDelegate(() => {
-            button.dispose();
-        });
-    }
+    return new DisposableDelegate(() => {
+      button.dispose();
+    });
+  }
 }
 
 export class BoardConnector extends ReactWidget {
-    constructor(sessionContext: ISessionContext) {
-        super();
-        this._board = "Board"
-        this.addClass(TOOLBAR_KERNEL_NAME_CLASS);
-        this._onStatusChanged(sessionContext);
-        sessionContext.statusChanged.connect(this._onStatusChanged, this);
-        sessionContext.connectionStatusChanged.connect(
-          this._onStatusChanged,
-          this
-        );
-        this._kernelModel = new KernelModel(sessionContext);
+  constructor(sessionContext: ISessionContext) {
+    super();
+    this._board = "Board"
+    this.addClass(TOOLBAR_KERNEL_NAME_CLASS);
+    this._onStatusChanged(sessionContext);
+    sessionContext.statusChanged.connect(this._onStatusChanged, this);
+    sessionContext.connectionStatusChanged.connect(
+      this._onStatusChanged,
+      this
+    );
+    this._kernelModel = new KernelModel(sessionContext);
+  }
+
+  private callback = () => {
+    this._kernelModel.execute('arduino-cli board list');
+    console.log(this._kernelModel.boardList);
+  };
+
+  /**
+  * Handle a status on a kernel.
+  */
+  private _onStatusChanged(sessionContext: ISessionContext) {
+    this._kernel = sessionContext.kernelDisplayName;
+    this.update();
+  }
+
+  render() {
+    if (this._kernel === 'Arduino') {
+      return (
+        <>
+          <ToolbarButtonComponent
+            onClick={this.callback}
+            tooltip={'Connect to an Arduino board'}
+            label={this._board}
+          />
+          <boardOff.react stylesheet={'toolbarButton'} alignSelf={'normal'} height={'24px'} />
+        </>)
     }
-
-    private callback = () => {
-        this._kernelModel.execute('!dir');
-    };
-
-    /**
-    * Handle a status on a kernel.
-    */
-    private _onStatusChanged(sessionContext: ISessionContext) {
-        this._kernel = sessionContext.kernelDisplayName;
-        this.update();
+    else {
+      return <></>;
     }
+  }
 
-    render() {
-        if (this._kernel === 'Python 3') {
-            return (
-                <>
-                    <ToolbarButtonComponent
-                        onClick={this.callback}
-                        tooltip={'Connect to an Arduino board'}
-                        label={this._board}
-                    />
-                    <boardOff.react stylesheet={'toolbarButton'} alignSelf={'normal'} height={'24px'} />
-                </>)
-        }
-        else {
-            return <></>;
-        }
-    }
+  private _kernel: string;
+  private _board: string;
 
-
-    private _kernel: string;
-    private _board: string;
-
-    private _kernelModel: KernelModel;
+  private _kernelModel: KernelModel;
 }
 
 export class IoTArchitecturalSwitch extends ReactWidget {
 
-    constructor(widget: Notebook,
-        translator?: ITranslator
-    ) {
-        super();
-        this._trans = (translator || nullTranslator).load('jupyterlab');
-        this.addClass(IOTTOOLBAR_ARCHITECTURAL_CLASS);
-        this._notebook = widget;
-        widget.stateChanged.connect(this.update, this);
+  constructor(widget: Notebook,
+    translator?: ITranslator
+  ) {
+    super();
+    this._trans = (translator || nullTranslator).load('jupyterlab');
+    this.addClass(IOTTOOLBAR_ARCHITECTURAL_CLASS);
+    this._notebook = widget;
+    widget.stateChanged.connect(this.update, this);
+  }
+
+  /**
+   * Handle `change` events for the HTMLSelect component.
+   */
+  handleChange = (event: React.ChangeEvent<HTMLSelectElement>): void => {
+    if (event.target.value !== '-') {
+      this._notebook.model.metadata.set(ARCHITECTURAL_ELEMENT_KEY, event.target.value);
+      this.update();
+      this._notebook.activate();
     }
+  };
 
-    /**
-     * Handle `change` events for the HTMLSelect component.
-     */
-    handleChange = (event: React.ChangeEvent<HTMLSelectElement>): void => {
-        if (event.target.value !== '-') {
-            this._notebook.model.metadata.set(ARCHITECTURAL_ELEMENT_KEY, event.target.value);
-            this.update();
-            this._notebook.activate();
-        }
-    };
-
-    render() {
-        let value = '-';
-        if (this._notebook.model.metadata.get(ARCHITECTURAL_ELEMENT_KEY) != null) {
-            value = this._notebook.model.metadata.get(ARCHITECTURAL_ELEMENT_KEY).toString();
-        }
-        return (
-            <HTMLSelect
-                className={IOTTOOLBAR_ARCHITECTURAL_DROPDOWN_CLASS}
-                onChange={this.handleChange}
-                value={value}
-                aria-label={this._trans.__('IoT Architectural Element')}
-            >
-                <option value="-">-</option>
-                <option value="app-and-cloud">{this._trans.__('Application & Cloud Services')}</option>
-                <option value="device">{this._trans.__('Devices')}</option>
-                <option value="gateway">{this._trans.__('Gateway')}</option>
-            </HTMLSelect>
-        );
+  render() {
+    let value = '-';
+    if (this._notebook.model.metadata.get(ARCHITECTURAL_ELEMENT_KEY) != null) {
+      value = this._notebook.model.metadata.get(ARCHITECTURAL_ELEMENT_KEY).toString();
     }
+    return (
+      <HTMLSelect
+        className={IOTTOOLBAR_ARCHITECTURAL_DROPDOWN_CLASS}
+        onChange={this.handleChange}
+        value={value}
+        aria-label={this._trans.__('IoT Architectural Element')}
+      >
+        <option value="-">-</option>
+        <option value="app-and-cloud">{this._trans.__('Application & Cloud Services')}</option>
+        <option value="device">{this._trans.__('Devices')}</option>
+        <option value="gateway">{this._trans.__('Gateway')}</option>
+      </HTMLSelect>
+    );
+  }
 
-    private _trans: TranslationBundle;
-    private _notebook: Notebook;
+  private _trans: TranslationBundle;
+  private _notebook: Notebook;
 }
 
 export class KernelModel {
-    constructor(session: ISessionContext) {
-      this._sessionContext = session;
-    }
-  
-    get future(): Kernel.IFuture<
-      KernelMessage.IExecuteRequestMsg,
-      KernelMessage.IExecuteReplyMsg
-    > | null {
-      return this._future;
-    }
-  
-    set future(
-      value: Kernel.IFuture<
-        KernelMessage.IExecuteRequestMsg,
-        KernelMessage.IExecuteReplyMsg
-      > | null
-    ) {
-      this._future = value;
-      if (!value) {
-        return;
-      }
-      value.onIOPub = this._onIOPub;
-    }
-  
-    get output(): IOutput | null {
-      return this._output;
-    }
-  
-    get stateChanged(): ISignal<KernelModel, void> {
-      return this._stateChanged;
-    }
-  
-    execute(code: string): void {
-      if (!this._sessionContext || !this._sessionContext.session?.kernel) {
-        return;
-      }
-      this.future = this._sessionContext.session?.kernel?.requestExecute({
-        code
-      });
-    }
-  
-    private _onIOPub = (msg: KernelMessage.IIOPubMessage): void => {
-      const msgType = msg.header.msg_type;
-      switch (msgType) {
-        case 'execute_result':
-        case 'display_data':
-        case 'update_display_data':
-          this._output = msg.content as IOutput;
-          console.log(this._output);
-          this._stateChanged.emit();
-          break;
-        default:
-          break;
-      }
-      return;
-    };
-  
-    private _future: Kernel.IFuture<
-      KernelMessage.IExecuteRequestMsg,
-      KernelMessage.IExecuteReplyMsg
-    > | null = null;
-    private _output: IOutput | null = null;
-    private _sessionContext: ISessionContext;
-    private _stateChanged = new Signal<KernelModel, void>(this);
+  constructor(session: ISessionContext) {
+    this._sessionContext = session;
   }
+
+  get future(): Kernel.IFuture<
+    KernelMessage.IExecuteRequestMsg,
+    KernelMessage.IExecuteReplyMsg
+  > | null {
+    return this._future;
+  }
+
+  set future(
+    value: Kernel.IFuture<
+      KernelMessage.IExecuteRequestMsg,
+      KernelMessage.IExecuteReplyMsg
+    > | null
+  ) {
+    this._future = value;
+    if (!value) {
+      return;
+    }
+    value.onIOPub = this._onIOPub;
+  }
+
+  get output(): IOutput | null {
+    return this._output;
+  }
+
+  get stateChanged(): ISignal<KernelModel, void> {
+    return this._stateChanged;
+  }
+
+  get boardList(): string[] {
+    return this._boardList;
+  }
+
+  execute(code: string): void {
+    if (!this._sessionContext || !this._sessionContext.session?.kernel) {
+      return;
+    }
+    this.future = this._sessionContext.session?.kernel?.requestExecute({
+      code
+    });
+  }
+
+  private _onIOPub = (msg: KernelMessage.IIOPubMessage): void => {
+    const msgType = msg.header.msg_type;
+    switch (msgType) {
+      case 'stream':
+        this._output = msg.content as IOutput;
+        var result = this._output.text.toString().split('Name ')[1];
+        if (result != '') {
+          this._boardList = result.split('\n').filter(obj => obj !== '');
+        }
+      case 'execute_result':
+      case 'display_data':
+      case 'update_display_data':
+        this._output = msg.content as IOutput;
+        this._stateChanged.emit();
+        break;
+      default:
+        break;
+    }
+    return;
+  };
+
+  private _future: Kernel.IFuture<
+    KernelMessage.IExecuteRequestMsg,
+    KernelMessage.IExecuteReplyMsg
+  > | null = null;
+  private _output: IOutput | null = null;
+  private _sessionContext: ISessionContext;
+  private _stateChanged = new Signal<KernelModel, void>(this);
+  private _boardList: string[];
+}
